6654689be1ae7a1ea1e31d0237d0406778a17c08
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-01-17 20:06:02 +0900

    게시판 뷰 기본 골격 작성

--- src/test/java/com/studyproject/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/studyproject/boardproject/controller/ArticleControllerTest.java
@@ -24,7 +24,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
     @DisplayName("[View/GET] 게시글 리스트 (게시판) 페이지 - 정상 호출 ")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -33,7 +32,7 @@ class ArticleControllerTest {
         // When&Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -49,7 +48,7 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComment"));
     }
 
