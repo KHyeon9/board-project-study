595257951f476e80baa7e765acc45fc661db8927
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-01-19 00:50:03 +0900

    User Account관련 도메인과 레포지토리 생성 및 data 직접 삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,20 @@
+-- user account
+insert into user_account (user_id, user_password, nick_name, email, memo, created_at, created_by, modified_at, modified_by)
+values ('hyeon', 'dummypassword', 'Hyeon', 'hyeon@mail.com', 'Hyeon', now(), 'hyeon', now(), 'hyeon');
+
 -- article 11
 insert into article(title, content, hashtag, created_by, created_at, modified_by, modified_at)
 values ('test1', 'content1', 'hashtag1', 'hyeon', '2022-01-12', 'hyeon', '2022-01-12'),
-        ('test2', 'content2', 'hashtag2', 'hyeon', '2022-01-13', 'hyeon', '2022-01-13'),
-        ('test3', 'content3', 'hashtag3', 'hyeon', '2022-01-14', 'hyeon', '2022-01-14'),
-        ('test4', 'content4', 'hashtag4', 'hyeon', '2022-01-15', 'hyeon', '2022-01-15'),
-        ('test5', 'content5', 'hashtag5', 'hyeon', '2022-01-16', 'hyeon', '2022-01-16'),
-        ('test6', 'content6', 'hashtag6', 'hyeon', '2022-01-17', 'hyeon', '2022-01-17'),
-        ('test7', 'content7', 'hashtag7', 'hyeon', '2022-01-18', 'hyeon', '2022-01-18'),
-        ('test8', 'content8', 'hashtag8', 'hyeon', '2022-01-19', 'hyeon', '2022-01-19'),
-        ('test9', 'content9', 'hashtag9', 'hyeon', '2022-01-20', 'hyeon', '2022-01-20'),
-        ('test10', 'content10', 'hashtag10', 'hyeon', '2022-01-21', 'hyeon', '2022-01-21');
+       ('test2', 'content2', 'hashtag2', 'hyeon', '2022-01-13', 'hyeon', '2022-01-13'),
+       ('test3', 'content3', 'hashtag3', 'hyeon', '2022-01-14', 'hyeon', '2022-01-14'),
+       ('test4', 'content4', 'hashtag4', 'hyeon', '2022-01-15', 'hyeon', '2022-01-15'),
+       ('test5', 'content5', 'hashtag5', 'hyeon', '2022-01-16', 'hyeon', '2022-01-16'),
+       ('test6', 'content6', 'hashtag6', 'hyeon', '2022-01-17', 'hyeon', '2022-01-17'),
+       ('test7', 'content7', 'hashtag7', 'hyeon', '2022-01-18', 'hyeon', '2022-01-18'),
+       ('test8', 'content8', 'hashtag8', 'hyeon', '2022-01-19', 'hyeon', '2022-01-19'),
+       ('test9', 'content9', 'hashtag9', 'hyeon', '2022-01-20', 'hyeon', '2022-01-20'),
+       ('test10', 'content10', 'hashtag10', 'hyeon', '2022-01-21', 'hyeon', '2022-01-21');
+
 -- article comment 15
 insert into article_comment(article_id, content, created_by, created_at, modified_by, modified_at)
 values  (1, 'content1', 'hyeon', '2022-01-12', 'hyeon', '2022-01-12'),
