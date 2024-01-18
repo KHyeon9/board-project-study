1070d1274075b27f4f79ae2e9e7edfed26737e0d
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-01-19 01:03:42 +0900

    이전에 사용하던 formLogin 방식을 최신 방식으로 변경

--- src/main/java/com/studyproject/boardproject/config/SecurityConfig.java
+++ src/main/java/com/studyproject/boardproject/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
@@ -12,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin().and()
+                .formLogin(withDefaults())
                 .build();
     }
 }
