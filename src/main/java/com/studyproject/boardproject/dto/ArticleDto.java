a829fdd910701211a5daa723c573cfa78ef6b302
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-01-19 21:08:06 +0900

    ArticleDto 생성

---
+++ src/main/java/com/studyproject/boardproject/dto/ArticleDto.java
@@ -0,0 +1,16 @@
+package com.studyproject.boardproject.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createAt,
+        String createBy
+) implements Serializable {
+    public static ArticleDto of(String title, String content, String hashtag, LocalDateTime createAt, String createBy) {
+        return new ArticleDto(title, content, hashtag, createAt, createBy);
+    }
+}
