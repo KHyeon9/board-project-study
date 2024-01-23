afe8ffe9b3d551120253d9047751e9ef53e7f0c6
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-01-23 21:06:38 +0900

    연관된 DTO 재설계 및 생성

--- src/main/java/com/studyproject/boardproject/dto/ArticleDto.java
+++ src/main/java/com/studyproject/boardproject/dto/ArticleDto.java
@@ -1,16 +1,44 @@
 package com.studyproject.boardproject.dto;
 
-import java.io.Serializable;
+import com.studyproject.boardproject.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleDto(
+        Long id,
+        UserAccountDto userAccountDto,
         String title,
         String content,
         String hashtag,
-        LocalDateTime createAt,
-        String createBy
-) implements Serializable {
-    public static ArticleDto of(String title, String content, String hashtag, LocalDateTime createAt, String createBy) {
-        return new ArticleDto(title, content, hashtag, createAt, createBy);
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
     }
 }
