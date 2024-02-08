91d3386a1d27847c677eb681d956b0f435a12ca6
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-02-08 18:19:34 +0900

    대댓글 비지니스 로직 구현

--- src/main/java/com/studyproject/boardproject/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/studyproject/boardproject/dto/response/ArticleCommentResponse.java
@@ -3,17 +3,30 @@ package com.studyproject.boardproject.dto.response;
 import com.studyproject.boardproject.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
         String content,
         String userId,
         String nickname,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments,
         String email,
         LocalDateTime createdAt
 ) {
     public static ArticleCommentResponse of(Long id, String content, String userId, String nickname, String email, LocalDateTime createdAt) {
-        return new ArticleCommentResponse(id, content, userId, nickname, email, createdAt);
+        return ArticleCommentResponse.of(id, content, userId, nickname, null, email, createdAt);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, String userId, String nickname, Long parentCommentId, String email, LocalDateTime createdAt) {
+        // 대댓글이 시간의 순서대로 출력되야 하므로 순서를 셋팅하는 로직
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt) // 시간의 순서대로 , 반대로 할경우 reverse()를 사용
+                .thenComparingLong(ArticleCommentResponse::id); // 만약 시간이 완전히 똑같을 경우 아이디로 순서를 적용
+        return new ArticleCommentResponse(id, content, userId, nickname, parentCommentId, new TreeSet<>(childCommentComparator), email, createdAt);
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -23,13 +36,18 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
                 dto.userAccountDto().userId(),
                 nickname,
+                dto.parentCommentId(),
                 dto.userAccountDto().email(),
                 dto.createdAt()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 }
--- src/main/java/com/studyproject/boardproject/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/studyproject/boardproject/dto/response/ArticleWithCommentsResponse.java
@@ -1,11 +1,12 @@
 package com.studyproject.boardproject.dto.response;
 
+import com.studyproject.boardproject.dto.ArticleCommentDto;
 import com.studyproject.boardproject.dto.ArticleWithCommentsDto;
 import com.studyproject.boardproject.dto.HashtagDto;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -42,10 +43,36 @@ public record ArticleWithCommentsResponse(
                 dto.userAccountDto().userId(),
                 nickname,
                 dto.userAccountDto().email(),
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new)),
+                organizeChildComments(dto.articleCommentDtos()),
                 dto.createdAt()
         );
+    }
+
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        // [articleComment의 id, ArticleCommentResponse] 로 매핑
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+        // 부모 comment가 있을 경우 자식 comment를 부모 comment의 Set에 포함
+        // [articleComment의 id, ArticleCommentResponse.childComments안에 포함]
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+                    parentComment.childComments().add(comment);
+                });
+        
+        // 댓글 순서를 정렬
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment())
+                .collect(Collectors.toCollection(() ->
+                        // 순서를 가져야 하므로 TreeSet사용
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed()
+                                .thenComparing(ArticleCommentResponse::id)
+                        )
+                    ));
     }
 }
